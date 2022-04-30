ffa304cced3711512423e72e8c417a1d923f6df0
helderlim <helderfelima16@gmail.com>
2022-04-30 13:52:30 -0300

    feat: primeiro post para o banco de dados

--- api/controllers/feed.js
+++ api/controllers/feed.js
@@ -1,5 +1,7 @@
 const { validationResult } = require('express-validator/check')
 
+const Post = require('../models/post')
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -29,14 +31,18 @@ exports.createPost = (req, res, next) => {
   }
   const title = req.body.title;
   const content = req.body.content;
-  res.status(201).json({
-    message: 'Post created sucessfully',
-    post: {
-      _id: new Date().toISOString(),
-      title: title,
-      content: content,
-      creator: { name: 'helder lima' },
-      createdAt: new Date()
-    }
-  })
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: 'https://images.unsplash.com/photo-1651243960650-e01bb3102ec9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    creator: { name: 'helder lima' },
+  });
+  post.save()
+    .then( result => {
+      res.status(201).json({
+        message: 'Post created sucessfully',
+        post: result
+      })
+    })
+    .catch( err => console.log(err));
 }
