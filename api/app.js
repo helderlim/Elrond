66068c3fbd853b2975a3f1bb74cae84c0af833af
helderlim <helderfelima16@gmail.com>
2022-04-30 14:17:51 -0300

    feat: satic images && error handling

--- api/app.js
+++ api/app.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 const express = require('express')
 const bodyParsed = require('body-parser')
 const feedRoutes = require('./routes/feed')
@@ -7,6 +9,7 @@ const app = express();
 
 // app.use(bodyParsed.urlencoded())
 app.use(bodyParsed.json());
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 //midleware - header da requisição
 app.use((req, res, next) => {
@@ -17,6 +20,13 @@ app.use((req, res, next) => {
 });
 
 app.use('/feed', feedRoutes)
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({message: message})
+})
 
 mongoose.connect('mongodb+srv://Elrond:xv0beXobWUrVmh30@cluster0.qatt4.mongodb.net/messages?retryWrites=true&w=majority')
   .then(result => {
--- api/routes/feed.js
+++ api/routes/feed.js
@@ -9,7 +9,7 @@ router.get('/posts', feedController.getPosts);
 router.post('/post', [
   body('title')
     .trim()
-    .isLength({min: 5}),
+    .isLength({min: 5 }),
     body('content')
       .trim()
       .isLength({min: 5})
